dc585b0919be878c9981c946d9088adb479e570b
Dunyafm <dunyafm@code.edu.az>
2022-02-22 16:19:51 -0800

    Sql used data types

---
+++ SQLQuery2-practice.sql
@@ -0,0 +1,34 @@
+--use P126
+--CREATE database P126
+--DROP DATABASE P126
+
+--Create table Students (
+--  Id int,
+--  [Name] nvarchar(50),
+--  Surname nvarchar(50),
+--  Age int
+
+--)
+
+--Drop table Students
+--Alter table Studnets
+--drop column [Address]
+
+--Alter table Students
+--add [Address] nvarchar(100),[Email] nvarchar(200)
+
+
+
+--exec sp_rename Students,Student
+
+
+--insert into Students (Id,Name,Surname,Age)
+--values (2,'Dunya','Mammadli',22)
+
+--select [Name],Surname from Students
+
+--select * from Students where Id > 3
+
+--select * from Students where Age > 20
+
+--update Students set Age = 25 where Id = 3
